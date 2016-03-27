7b6f779666bd8a42ce8b540aa4d98043e5befa6b
cyzhang <zhangchenyang89524@gmail.com>
2016-03-27 17:59:29 -0400

    first commit

--- write_csv.py
+++ write_csv.py
@@ -13,13 +13,38 @@ with open(sys.argv[1], 'r') as fp:
 
 # read the orignial csv and write the seq.
 all_row = []
-with open('example.csv', 'r') as fp:
+with open('./data/example.csv', 'r') as fp:
     reader = csv.reader(fp, delimiter=',')
     for in_row in reader:
         all_row.append(in_row)
 
-with open('output.csv','w') as fp:
-    reader = csv.writer(fp, delimiter=',')
-    for row in all_rows:
-        key_list = 
+p = {}
+p['C'] = 'G'
+p['G'] = 'C'
+p['A'] = 'T'
+p['T'] = 'A'
+file_n = 0
+with open('./data/output.csv','w') as fp:
+    writer = csv.writer(fp, delimiter=',')
+    for row in all_row:
+        if file_n == 0:
+            file_n = 1
+            continue
+        chid = row[0][3:]
+        start = row[1]
+        end = row[2]
+        label = row[4]
+        key = '-'.join([chid, start, end, label])
+        if key in seq_lines:
+            seq = seq_lines[key]
+        else:
+            print "not right"
+            exit(-1)
+        if label == 'Rev':
+            seq_copy = seq
+            seq = [p[c] for c in seq_copy[::-1]]
+            seq = ''.join(seq)
+        row.append(seq)
+        writer.writerow(row)
+
 
